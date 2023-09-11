e115f001cbbfea6801bdfc08dae6e647da2a9ac0
Jibea <guylain.thomas@epitech.eu>
2023-09-11 17:01:37 +0200

    fix(index.js): reset the isSigned value

--- SurvivorNext/Index.js
+++ SurvivorNext/Index.js
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 
 export default function ConnectionChecker()
 {
-    const [isSignedIn, setIsSignedIn] = useState(true);
+    const [isSignedIn, setIsSignedIn] = useState(false);
     const [ApiUser, setApiUser] = useState({});
 
     if (isSignedIn) {

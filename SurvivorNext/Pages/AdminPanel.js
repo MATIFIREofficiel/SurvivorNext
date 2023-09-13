d3586ac05dfcad446c8c09368c5f91e7b69a4b7b
matthias <matthias.gaste@epitech.eu>
2023-09-14 00:44:31 +0200

    feat(Admin Panel): add front of the admin Panel

--- SurvivorNext/Pages/AdminPanel.js
+++ SurvivorNext/Pages/AdminPanel.js
@@ -1,97 +1,160 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Button, Image, TouchableOpacity, StyleSheet, Platform } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
+import React, { useState, useRef } from 'react';
+import {
+  Modal,
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  StyleSheet,
+  TextInput,
+  Image,
+  TouchableWithoutFeedback,
+  Button,
+} from 'react-native';
 
-export default function AdminPanel()
-{
-  const [companyName, setCompanyName] = useState("Nom de l'entreprise par défaut");
-  const [editing, setEditing] = useState(false);
-  const [imageUri, setImageUri] = useState(null);
-  const [theme, setTheme] = useState('light');
+export default function AdminPanel() {
+  const buttonColors = ['red', 'orange', 'yellow', 'blue', 'indigo', 'violet', 'purple', 'magenta', 'lime']; // Couleurs des boutons
+  const [selectedColor, setSelectedColor] = useState(null);
 
-  const handleSave = () => {
-    setEditing(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [companyName, setCompanyName] = useState('');
+
+  const toggleModal = () => {
+    setIsModalVisible(!isModalVisible);
+  };
+  const handleButtonPress = (color) => {
+    console.log(`Couleur du bouton : ${color}`);
+    setSelectedColor(color);
   };
 
-  const handleImageUpload = async () => {
-    try {
-      let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.All,
-        allowsEditing: true,
-        aspect: [4, 3],
-        quality: 1,
-      });
-      if (!result.cancelled) {
-        setImageUri(result.uri);
-      }
-    } catch (error) {
-      console.error('Erreur lors de la sélection de l\'image :', error);
-    }
+  const handleCompanyNameChange = (text) => {
+    setCompanyName(text);
   };
-  const changeTheme = (newTheme) => {
-    setTheme(newTheme);
+
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
+  const imagePickerRef = useRef();
+
+  const handleImagePress = () => {
   };
 
   return (
-    <View style={[styles.container, { backgroundColor: theme === 'dark' ? 'black' : 'white' }]}>
-    <TouchableOpacity onPress={handleImageUpload} style={styles.imageUploadButton}>
-      {imageUri ? (
-        <Image source={{ uri: imageUri }} style={styles.image} />
-      ) : (
-        <Text>Ajouter une photo</Text>
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.TextInput}
+        onPress={toggleModal}
+      >
+        <Text>Company name</Text>
+      </TouchableOpacity>
+      {isModalVisible && (
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={isModalVisible}
+          onRequestClose={() => {
+            toggleModal();
+          }}
+        >
+          <View style={styles.modalView}>
+            <TextInput
+              style={styles.modalTextInput}
+              value={companyName}
+              onChangeText={setCompanyName}
+              placeholder="Undefined"
+            />
+            <Button
+              title="Enregistrer"
+              onPress={() => {
+                toggleModal();
+              }}
+            />
+          </View>
+        </Modal>
       )}
-    </TouchableOpacity>
-    <Text style={[styles.companyName, { color: theme === 'dark' ? 'white' : 'black' }]}>{companyName}</Text>
-    {editing ? (
-      <View>
-        <TextInput
-          style={styles.input}
-          value={companyName}
-          onChangeText={(text) => setCompanyName(text)}
-        />
-        <Button title="Enregistrer" onPress={handleSave} />
+      <TouchableWithoutFeedback onPress={handleImagePress}>
+        <View style={styles.Image}>
+          {selectedPhoto ? (
+            <Image source={{ uri: selectedPhoto }} style={styles.selectedImage} />
+          ) : (
+            <Text>Select a photo</Text>
+          )}
+        </View>
+      </TouchableWithoutFeedback>
+      <View style={styles.buttonRow}>
+        {buttonColors.map((color, index) => (
+          <TouchableOpacity
+            key={index}
+            style={[
+              styles.button,
+              { backgroundColor: color },
+              selectedColor === color ? { borderWidth: 7, borderColor: 'black' } : { borderWidth: 2, borderColor: 'black' },
+            ]}
+            onPress={() => handleButtonPress(color)}
+          >
+            <Text style={styles.buttonText}>{color}</Text>
+          </TouchableOpacity>
+        ))}
       </View>
-    ) : (
-      <Button title="Modifier le nom de l'entreprise" onPress={() => setEditing(true)} />
-    )}
-
-    <View style={styles.themeButtons}>
-      <Button title="Thème clair" onPress={() => changeTheme('light')} />
-      <Button title="Thème sombre" onPress={() => changeTheme('dark')} />
     </View>
-  </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    padding: 20,
+    justifyContent: 'flex-end',
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  button: {
+    width: '30%',
+    height: '15%',
+    marginVertical: 10,
     alignItems: 'center',
-    padding: 16,
+    justifyContent: 'center',
+    borderRadius: 10,
   },
-  companyName: {
-    fontSize: 24,
-    marginBottom: 16,
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
   },
-  input: {
-    borderWidth: 1,
-    borderColor: 'gray',
-    padding: 8,
-    marginBottom: 16,
-  },
-  imageUploadButton: {
-    width: 150,
-    height: 150,
-    borderRadius: 75,
+  Image: {
+    width: 200,
+    height: 200,
     backgroundColor: 'lightgray',
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 16,
+    borderRadius: 100,
+    alignSelf: 'center',
+    marginTop: 20,
+    marginBottom: 100,
+    justifyContent: 'center',
   },
-  image: {
-    width: 150,
-    height: 150,
-    borderRadius: 75,
+  TextInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 80,
+    paddingLeft: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modalView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  modalTextInput: {
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 10,
+    borderRadius: 5,
   },
 });

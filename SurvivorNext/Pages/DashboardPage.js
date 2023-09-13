97a300e3046874e6cd6ffd765258ef48e6cb93e2
Jibea <87119218+Jibea@users.noreply.github.com>
2023-09-13 12:33:37 +0200

    Update SurvivorNext/Pages/DashboardPage.js
    Co-authored-by: Matthias Gst <matthias.gaste@epitech.eu>

--- SurvivorNext/Pages/DashboardPage.js
+++ SurvivorNext/Pages/DashboardPage.js
@@ -124,7 +124,6 @@ export default function DashboardPage() {
         snapPoints={snapPoints}
         detached={true}
         enablePanDownToClose={true}
-        // handleIndicatorPress={handleIndicatorPress}
         style={{ zIndex: 1 }}
       >
         <FlatList

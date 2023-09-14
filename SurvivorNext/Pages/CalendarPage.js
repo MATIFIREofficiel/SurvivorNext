9ede3c24c372798c611b711c8353e66147ea5b68
ThisMa <91668942+MathisCaugant@users.noreply.github.com>
2023-09-14 09:42:35 +0200

    feat(Calendar): add a calendar in the drawer CalendarPage (#51)
    * feat(Calendar): add a calendar in the drawer CalendarPage
    
    * Update SurvivorNext/Pages/CalendarPage.js
    
    Co-authored-by: Matthias Gst <matthias.gaste@epitech.eu>
    
    * Update SurvivorNext/Pages/CalendarPage.js
    
    Co-authored-by: Matthias Gst <matthias.gaste@epitech.eu>
    
    * Update SurvivorNext/Pages/CalendarPage.js
    
    Co-authored-by: Matthias Gst <matthias.gaste@epitech.eu>
    
    ---------
    
    Co-authored-by: MathisCaugant <mathis.caugant@epitec.eu>
    Co-authored-by: Matthias Gst <matthias.gaste@epitech.eu>

---
+++ SurvivorNext/Pages/CalendarPage.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import {Calendar, LocaleConfig} from 'react-native-calendars';
+
+LocaleConfig.locales['en'] = {
+    monthNames: [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ],
+    monthNamesShort: ['Jan.', 'Feb.', 'March', 'April', 'May', 'June', 'July', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.'],
+    dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    dayNamesShort: ['Sun.', 'Mon.', 'Tue.', 'Wed.', 'Thu.', 'Fri.', 'Sat.'],
+    today: "Today"
+};
+
+LocaleConfig.defaultLocale = 'fr';
+    
+export default function CalendarPage() {
+    const [selected, setSelected] = useState('');
+
+    return (
+        <Calendar
+            onDayPress={day => {setSelected(day.dateString);}}
+            markedDates={{[selected]: {selected: true, disableTouchEvent: true, selectedDotColor: 'orange'}}}
+            style={{
+                borderRadius: 5,
+                margin: 12,
+            }}
+            theme={{
+                calendarBackground: '#222',
+                dayTextColor: '#fff',
+                textDisabledColor: '#444',
+                monthTextColor: '#888'
+            }}
+        />
+    )
+}

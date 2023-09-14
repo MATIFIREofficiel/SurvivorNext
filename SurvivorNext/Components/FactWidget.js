529420aab0c6f3fd4ed548d117d9954306cdf28a
MatteoTurpault <91669387+TurpaultM@users.noreply.github.com>
2023-09-14 14:20:11 +0200

    feat(widget): add fact widget (#66)
    * feat(widget): add currency convert
    
    * feat(tests): add login page test and configuration files for bun
    
    * feat(tests): add tests for LoginPage
    
    * refactor(tests): remove useless
    
    * refactor(tests): remove useless
    
    * feat(tests): add tests for profile and trombinoscope pages
    
    * feat(widget): add jokes widget
    
    * refactor(tests): remove tests
    
    * feat(widget): add fact widget
    
    * feat(widget): remove radiusborder

---
+++ SurvivorNext/Components/FactWidget.js
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, Modal, StyleSheet } from 'react-native';
+import axios from 'axios';
+
+const FactApp = () => {
+    const [fact, setFact] = useState('');
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const fetchFact = async () => {
+        try {
+            const response = await axios.get('https://uselessfacts.jsph.pl/random.json');
+            setFact(response.data.text);
+            setModalVisible(true);
+        } catch (error) {
+            console.error('Error', error);
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.header}>Facts</Text>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={fetchFact}
+            >
+                <Text style={styles.buttonText}>New fact</Text>
+            </TouchableOpacity>
+            <Modal
+                visible={modalVisible}
+                transparent={true}
+                animationType="slide"
+                onRequestClose={() => setModalVisible(false)}
+            >
+                <View style={styles.modalContainer}>
+                    <Text style={styles.factText}>{fact}</Text>
+                    <TouchableOpacity
+                        style={styles.closeButton}
+                        onPress={() => setModalVisible(false)}
+                    >
+                        <Text style={styles.closeButtonText}>Close</Text>
+                    </TouchableOpacity>
+                </View>
+            </Modal>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        height: 255,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        borderRadius: 10,
+    },
+    header: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    button: {
+        backgroundColor: 'blue',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    },
+    factText: {
+        fontSize: 20,
+        textAlign: 'center',
+        marginBottom: 30,
+        paddingHorizontal: 20,
+        color: 'white',
+    },
+    closeButton: {
+        backgroundColor: 'red',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 5,
+        marginTop: 20,
+    },
+    closeButtonText: {
+        color: 'white',
+        fontSize: 16,
+    },
+});
+
+export default FactApp;

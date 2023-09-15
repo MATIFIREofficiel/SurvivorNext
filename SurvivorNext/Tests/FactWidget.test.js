9c67b627684e3a8afb9e462e225eaa74035fe5f4
TurpaultM <matteo.turpault@epitech.eu>
2023-09-15 08:48:15 +0200

    feat(test): add test for joke widget

---
+++ SurvivorNext/Tests/FactWidget.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react-native';
+import FactApp from '../Components/FactWidget';
+import { useAppContext } from '../AppContext';
+
+jest.mock('../AppContext', () => ({
+  useAppContext: () => ({
+    appColor: 'blue',
+  }),
+}));
+
+describe('FactApp Tests', () => {
+  it('affiche le texte du bouton', () => {
+    const { getByText } = render(<FactApp />);
+    const buttonText = getByText('New fact');
+    expect(buttonText).toBeTruthy();
+  });
+
+  it('ferme la modal', async () => {
+    const { getByText, getByTestId, queryByTestId } = render(<FactApp />);
+    const fetchFactButton = getByText('New fact');
+    fireEvent.press(fetchFactButton);
+
+    await waitFor(() => {
+      const closeButton = getByText('Close');
+      fireEvent.press(closeButton);
+    });
+
+    await waitFor(() => {
+      const modal = queryByTestId('modal');
+      expect(modal).toBeNull();
+    });
+  });
+});
